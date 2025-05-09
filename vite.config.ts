82f647d6d667074d355885abda0a37e2c858be8c
German Salazar <salazar.germandiego@gmail.com>
2025-05-09 15:29:55 -0400

    cambio de base

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
     build: {
         outDir: 'build',
     },
-=======
->>>>>>> refs/remotes/origin/main
     base: '/todo/',
     plugins: [react()],
 });
